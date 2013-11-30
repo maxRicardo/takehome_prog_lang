c513565bd487a58d26e8f856d9f7fd05e8453f65
maxRicardo <xaradrim@gmail.com>
2013-11-30 00:49:56 -0400

    First Commit and import to repository

---
+++ TakeHomeFinal/src/Problem1/binaryreader.py
@@ -0,0 +1,22 @@
+'''
+Author: Max R. Berrios      
+Description: Bioinformatician 
+Date:  Nov 30, 2013  
+Purpose: 
+'''
+
+import re
+
+
+'''
+Function will determine if the 
+given String is a even 
+binary number or not.
+@param string: 
+@return: Boolean 
+'''
+
+def evenBinarReader(string):
+    
+    
+    
