2be29b957fbb4d697b6835ecf7d0a86a91940da3
maxRicardo <xaradrim@gmail.com>
2013-12-11 20:25:31 -0400

    Implementation of the problem 5 done. So far need to work the explanatin on the discussion

--- TakeHomeFinal/src/Problem3/Picasso_Imaging.py
+++ TakeHomeFinal/src/Problem3/Picasso_Imaging.py
@@ -10,12 +10,12 @@ from PIL import Image
 im = Image.open("dna.jpeg")
 
 try:
-    im.save("outpic.jpg","JPEG")
+    im.save("outpic.jpeg","JPEG")
 except:
     print "cannot print the pic"
 
 print im.format,im.size,im.mode
 
-im = Image.open("outpic.jpg").convert("RGB")
+im = Image.open("dna.jpeg").convert("RGB")
 im.show()
 
